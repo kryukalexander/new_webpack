a21d05c6f7dfc58bd9049d1120695be9753f9106
Alexander Kryuk <kryukalexander@gmail.com>
2018-12-12 10:37:14 +0600

    Added npm script to run webpack dev-server with hmr for stylesheets and scripts

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,6 @@
 import '../css/style.scss';
 import './main.js';
 
-// todo add errors handling
 if (module.hot) {
     module.hot.accept();
 }
--- webpack/hotReload.js
+++ webpack/hotReload.js
@@ -1,13 +1,7 @@
-//todo https://github.com/jantimon/html-webpack-plugin/issues/100#issuecomment-368303060
-
 const webpack = require('webpack');
 
 module.exports = function() {
     return {
-        devServer: {
-            hot: true,
-        },
-
         plugins: [
             new webpack.HotModuleReplacementPlugin()
         ]
