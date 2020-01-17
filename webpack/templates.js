0516544f74cbba066b1c4f08bf53bf78e7e63c8d
Alexander Kryuk <kryukalexander@gmail.com>
2020-01-17 16:10:28 +0600

    Template generation plugins array moved to templates.js

--- webpack/templates.js
+++ webpack/templates.js
@@ -1,3 +1,14 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const pages = ['index'];
+
+const pluginsList = pages.map( page => {
+    const options = {
+        template: `./templates/${page}.html`,
+        filename: `./${page}.html`
+    };
+    return new HtmlWebpackPlugin(options);
+});
+
 module.exports = function() {
     return {
         module: {
@@ -11,7 +22,9 @@ module.exports = function() {
                         }
                     ]
                 },
-            ]
-        }
+            ],
+        },
+
+        plugins: pluginsList
     }
 };
